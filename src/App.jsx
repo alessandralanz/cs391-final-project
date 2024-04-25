ae3a0f0f9f6907db2f5abb824de6e988943733f3
Alessandra Lanz <alanz@bu.edu>
2024-04-24 23:51:03 -0400

    Fixed the routes for each page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import Home from "./components/home.jsx"
-import Navbar from "./components/navbar.jsx"
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from "./components/home.jsx";
+import Fenway from "./components/fenway.jsx";
+import Crc from './pages/crc.jsx';
+import NavBar from "./components/navbar.jsx";
 
 function App() {
   return (
-      <>
-      <Navbar/>
-          <Home/>
-
-      </>
-  )
+      <Router>
+      <NavBar/>
+          <Routes>
+              <Route exact path="/" element={<Home/>}/>
+              <Route path="/fenway" element={<Fenway/>}/>
+              <Route path="/Crc" element={<Crc/>}/>
+          </Routes>
+      </Router>
+  );
 }
 
 export default App
